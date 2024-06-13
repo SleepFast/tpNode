b78ff5fd55b1ff799cff1f69430ceb5ebf648dd8
S-Keyp <s-keyp@outlook.fr>
2024-06-13 15:00:23 +0200

    feat: route to create beer

--- router/biereRouter.js
+++ router/biereRouter.js
@@ -3,8 +3,12 @@ const controller = require("../controller/biereController")
 const express = require('express')
 const router = express.Router()
 
+router.post("/bars/:id/biere", controller.create)
 router.get("/biere/:id", controller.getById)
 router.put("/biere/:id", controller.update)
 router.delete("/biere/:id", controller.delete)
 
 module.exports = router
+/**
+  GET /bars/:id_bar/biere => Liste des bières d'un bar
+ */
