e58d41e7c26ff9c32fc3254de9ba91f825a8a71d
S-Keyp <s-keyp@outlook.fr>
2024-06-13 12:14:31 +0200

    fix: verifyToken + add protected route for bars

--- router/barsRouter.js
+++ router/barsRouter.js
@@ -1,12 +1,12 @@
 const controller = require("../controller/barsController")
-
+const { verifyToken } = require("../auth/auth")
 const express = require('express')
 const router = express.Router()
 
 router.post("/login", controller.login)
-router.post("/bars", controller.register)
-router.put("/bars/:id", controller.update)
-router.delete("/bars/:id", controller.delete)
+router.post("/bars", verifyToken, controller.register)
+router.put("/bars/:id", verifyToken, controller.update)
+router.delete("/bars/:id", verifyToken, controller.delete)
 router.get("/bars", controller.getAll)
 router.get("/bars/:id", controller.getById)
 
