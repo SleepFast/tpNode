8dfe7790dbbbdd4a3428851f99b7503348dcaabb
S-Keyp <s-keyp@outlook.fr>
2024-06-14 11:52:47 +0200

    feat: biere_commande router + Controller

--- controller/biere_commandeController.js
+++ controller/biere_commandeController.js
@@ -1,7 +1,44 @@
 const controller = {};
-
 const Biere_commande = require("../models/Biere_commande");
+const Biere = require("../models/Biere");
+const Commande = require("../models/Commande");
 
+controller.addBiereToCommande = (req, res) => {
+    const commandId = req.params.id;
+    const biereId = req.params.id_biere;
+
+    Biere.findByPk(biereId).then(() => {
+        Commande.findByPk(commandId).then(() => {
+            const biereCommand = {
+                biere_id: biereId,
+                commande_id: commandId,
+            }
+            Biere_commande.create(biereCommand).then((biereCommand) => {
+                res.status(201).send({ biereCommand, message: "Biere command created successfully" })
+            }).catch((error) => {
+                res.status(500).send({ message: "Error creating biere_command" })
+            })
+        }).catch((error) => {
+            res.status(400).send({ message: "Command not found" })
+        })
+    }).catch((error) => {
+        res.status(400).send({ message: "Biere not found" });
+    })
+
+
+}
+
+controller.removeBiereFromCommande = (req, res) => {
+    const commandId = req.params.id;
+    const biereId = req.params.id_biere;
+
+    Biere_commande.destroy({ where: { biere_id: biereId, commande_id: commandId } })
+        .then((deleteCount) => {
+            if (deleteCount === 0) return res.status(401).send({ message: "Cannot delete biere_command, biere_command not found" })
+            return res.send({ message: "Biere_command deleted !" });
+        })
+
+}
 
 
 module.exports = controller;
--- router/biere_commandeRouter.js
+++ router/biere_commandeRouter.js
@@ -4,4 +4,8 @@ const express = require('express')
 const router = express.Router()
 
 
+router.post("/commandes/:id/biere/:id_biere", controller.addBiereToCommande)
+router.delete("/commandes/:id/biere/:id_biere", controller.removeBiereFromCommande)
+// POST /commandes/:id/biere/:id => Ajouter une bière à une commande
+// DELETE /commandes/:id/biere/:id => Supprimer une bière d'une commande
 module.exports = router
